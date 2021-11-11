dd23dbf3918fe6319c84d855b5334a4f50e47c1c
Cameron1500 <cameronrichards1500@gmail.com>
2021-11-11 00:02:00 +0000

    Renamed util.py to tester.py

--- tester.py
+++ tester.py
@@ -1,10 +1,2 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-def fft(data, sample_rate):
-    fig, ax = plt.subplots()
-
-    _fft = abs(np.fft.fft(data))
-    _ft = np.linspace(0, sample_rate, len(_fft))
-
-    ax.plot(_ft, _fft)
