5f20797fdc2dc96e1cceeefb08606512e5d303bd
Cameron1500 <cameronrichards1500@gmail.com>
2021-12-16 01:11:21 +0000

    Sample rate code

--- realtime_iir_main.py
+++ realtime_iir_main.py
@@ -4,15 +4,18 @@ import iir_filter as iir
 import realtime_plot as rtp
 
 """ Constants """
-fs = 100   # Max 1000
+fs = 1000   # Max 1000
 fn = fs / 2
 
 # Cutoff Frequency (Hz)
 fc = 0.5
 
 """ Real Time Plotters """
-raw_data_plot = rtp.RealtimePlot(fs, 250, "Pre-Filter", show_fft = True)
-filter_data_plot = rtp.RealtimePlot(fs, 250, "Post-Filter", show_fft = True)
+raw_data_plot = rtp.RealtimePlot(fs, 250, "Pre-Filter")
+filter_data_plot = rtp.RealtimePlot(fs, 250, "Post-Filter")
+
+""" Sample Rate """
+sample_rate = rtp.SampleRate()
 
 """ IIR Filter """
 sos = signal.butter(1, fc / fn, "highpass", output="sos")
@@ -20,6 +23,7 @@ f = iir.IIR_filter(sos)
 
 """ Sample Process Function """
 def processSample(data):
+    sample_rate.tick()
     raw_data_plot.addSample(data)
 
     filter_data = f.filter(data)
