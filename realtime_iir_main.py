aaf04f081b2898976fb19dbe85bd4ee6596570a6
Cameron1500 <cameronrichards1500@gmail.com>
2021-12-22 19:37:43 +0000

    Added multiple channels to realtime_plot

--- realtime_iir_main.py
+++ realtime_iir_main.py
@@ -4,46 +4,40 @@ from scipy import signal
 import numpy as np
 
 import realtime_plot as rtp
-import gesture_detect as gd
 import iir_filter as iir
 
 """ Constants """
 # Arduino Sample Rate
-fs = 1000   # Max 1000
+fs = 100   # Max 1000
 # Nyquist
 fn = fs / 2
 
 """ IIR Filter Design """
 # DC Noise Removal (0.1Hz High-pass)
-sos_hp = signal.butter(2, 0.5 / fn, "highpass", output="sos")
-# Anti-Alising (~500Hz Low Pass)
-sos_lp = signal.butter(2, 5 / fn, "lowpass", output="sos")
-
-# Combine Filters
-sos = np.concatenate([sos_lp, sos_hp])
-f = iir.IIR_filter(sos)
-
-""" Gesture Detect """
-gesture_detector = gd.GestureDetect(0.005)
+sos = signal.butter(2, 0.1 / fn, "highpass", output="sos")
+x_filter = iir.IIR_filter(sos)
+y_filter = iir.IIR_filter(sos)
 
 """ Real Time Plotters """
-raw_data_plot = rtp.RealtimePlot(fs, 2, "Pre-Filter", fs_calc=True)
-filter_data_plot = rtp.RealtimePlot(fs, 2, "Post-Filter")
+raw = rtp.RealtimePlots(fs, 2, "Raw Data", channels=2)
+filtered = rtp.RealtimePlots(fs, 2, "Filtered Data", channels=2)
 
 """ Sample Process Function """
-def processSample(data):
-    raw_data_plot.addSample(data)
+def addX(data):
+    raw.addSample(data, channel=0)
+    filtered.addSample(x_filter.filter(data), channel=0)
 
-    filter_data = f.filter(data)
-    filter_data_plot.addSample(filter_data)
-
-    gesture_detector.processSample(filter_data)
+def addY(data):
+    raw.addSample(data, channel=1)
+    filtered.addSample(y_filter.filter(data), channel=1)
 
 """ Aurdino Data Aquisition """
 board = Arduino(Arduino.AUTODETECT)
 board.samplingOn(1000/fs)
-board.analog[0].register_callback(processSample)
+board.analog[0].register_callback(addY)
 board.analog[0].enable_reporting()
+board.analog[1].register_callback(addX)
+board.analog[1].enable_reporting()
 
 """ Show Real Time Plots """
 rtp.plt.show()
--- realtime_plot.py
+++ realtime_plot.py
@@ -3,20 +3,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-""" Real-Time Plotter """
-class RealtimePlot:
-    def __init__(self, fs, window_time, title, sample_limits=[-0.25, 0.25], fft_limits=[0,50], fs_calc=False):
+""" Rolling Buffer """
+class RollingBuffer:
+    def __init__(self, size):
+        self.size = size
+        self.plot_buffer = np.zeros(self.size)
+        self.data_buffer = []
+    
+    def update(self):
+        self.plot_buffer = np.append(self.plot_buffer, self.data_buffer)
+        self.plot_buffer = self.plot_buffer[-self.size:]
+        self.data_buffer = []
+        return self.plot_buffer
+    
+    def add(self, v):
+        self.data_buffer.append(v)
+
+""" Real-Time Plotter + FFT"""
+class RealtimePlotFFT:
+    def __init__(self, fs, window_time, title, sample_limits=[-2.5, 2.5], fft_limits=[0,50], fs_calc=False):
         # Buffer
         self.buffer_size = fs * window_time
-        self.plot_buffer = np.zeros(self.buffer_size)
-        self.data_buffer = []
+        self.buffer = RollingBuffer(self.buffer_size)
 
         # Figure Plot
         self.fig, self.ax = plt.subplots(nrows=2)
         
         # Sample Plot
         self.ax[0].plot([0, self.buffer_size-1],[0,0],color="r")
-        self.line_r, = self.ax[0].plot(self.plot_buffer)
+        self.line_r, = self.ax[0].plot(self.buffer.update())
         self.ax[0].set_ylim(sample_limits[0], sample_limits[1])
         self.ax[0].set_title(title)
 
@@ -38,10 +53,7 @@ class RealtimePlot:
 
     def update(self, x):
         # Buffer
-        self.plot_buffer = np.append(self.plot_buffer, self.data_buffer)
-        self.plot_buffer = self.plot_buffer[-self.buffer_size:]
-        self.data_buffer = []
-        self.line_r.set_ydata(self.plot_buffer)
+        self.line_r.set_ydata(self.buffer.update())
 
         if self.update_count % 5 == 0: # Reduce updates for performance
             # FFT
@@ -60,5 +72,55 @@ class RealtimePlot:
 
     def addSample(self, v):
         # Add Sample to Buffer
-        self.data_buffer.append(v)
+        self.buffer.add(v)
         self.sample_count += 1
+
+""" Real-Time Plotter 2 Channel """
+class RealtimePlots:
+    def __init__(self, fs, window_time, title, sample_limits=[-2.5, 2.5], channels=1):
+        # Buffer
+        self.buffer_size = fs * window_time
+        self.buffers = []
+
+        # Figure Plot
+        self.fig, self.ax = plt.subplots()
+        
+        # Sample Plot
+        self.ax.plot([0, self.buffer_size-1],[0,0],color="r")
+        self.ax.set_ylim(sample_limits[0], sample_limits[1])
+        self.ax.set_title(title)
+        self.lines = []
+
+        # Plot Buffers
+        for i in range(channels):
+            self.buffers.append(RollingBuffer(self.buffer_size))
+            line, = self.ax.plot(self.buffers[i].update())
+            self.lines.append(line)
+
+        self.anim = animation.FuncAnimation(self.fig, self.update, interval=100)
+        self.update_count = 0
+
+        # Sample Rate
+        self.sample_count = 0
+        self.label = self.ax.text(0, sample_limits[0], "Sample Rate: -", ha="left", va="bottom", fontsize=15)
+        self.last = 0
+
+    def update(self, x):
+        # Buffer
+        for i in range(len(self.lines)):
+            self.lines[i].set_ydata(self.buffers[i].update())
+        
+        # Sample Rate Calc
+        if self.update_count % 5 == 0: # Reduce updates for performance
+            current_time = time.time()
+            sample_rate = self.sample_count / (current_time - self.last)
+            self.last = current_time
+            self.sample_count = 0
+
+            self.label.set_text(f"Fs: {sample_rate:.1f}Hz")
+        self.update_count += 1
+
+    def addSample(self, v, channel=0):
+        if channel == 0:
+            self.sample_count += 1
+        self.buffers[channel].add(v)
