c77a8680849f25b9efed97c6129154d149544f55
Cameron1500 <cameronrichards1500@gmail.com>
2021-12-23 21:30:05 +0000

    Added third channel

--- Python Code/realtime_iir_main.py
+++ Python Code/realtime_iir_main.py
@@ -8,28 +8,55 @@ import iir_filter as iir
 
 """ Constants """
 # Arduino Sample Rate
-fs = 100   # Max 1000
+fs = 1000   # Max 1000
 # Nyquist
 fn = fs / 2
 
 """ IIR Filter Design """
-# DC Noise Removal (0.1Hz High-pass)
-sos = signal.butter(2, 0.1 / fn, "highpass", output="sos")
+# Noise Removal
+sos = signal.butter(2, 0.5 / fn, "lowpass", output="sos")
+
 x_filter = iir.IIR_filter(sos)
 y_filter = iir.IIR_filter(sos)
+z_filter = iir.IIR_filter(sos)
 
 """ Real Time Plotters """
-raw = rtp.RealtimePlots(fs, 2, "Raw Data", channels=2)
-filtered = rtp.RealtimePlots(fs, 2, "Filtered Data", channels=2)
+raw = rtp.RealtimePlots(fs, 2, "Raw Data", sample_limits=[-5,5], channels=3)
+filtered = rtp.RealtimePlots(fs, 2, "Filtered Data", sample_limits=[-5,5], channels=3)
+
+orientation = rtp.RealtimeVectorPlot()
+
+""" Convert Normalized Voltage to Acceleration """
+def v2a(n_volt):
+    # Normalised voltage to voltage and Re-centre (3.3V / 2 = 0g)
+    volts = (n_volt * 5) - (3.3 / 2)
+    # Convert to acceleration (300mV per g)
+    return volts / 0.3
 
 """ Sample Process Function """
 def addX(data):
-    raw.addSample(data, channel=0)
-    filtered.addSample(x_filter.filter(data), channel=0)
+    acc = v2a(data)
+    raw.addSample(acc, channel=0)
+
+    f_acc = x_filter.filter(acc)
+    orientation.addSample(acc, channel=0)
+    filtered.addSample(f_acc, channel=0)
 
 def addY(data):
-    raw.addSample(data, channel=1)
-    filtered.addSample(y_filter.filter(data), channel=1)
+    acc = v2a(data)
+    raw.addSample(acc, channel=1)
+
+    f_acc = y_filter.filter(acc)
+    orientation.addSample(f_acc, channel=1)
+    filtered.addSample(f_acc, channel=1)
+
+def addZ(data):
+    acc = v2a(data)
+    raw.addSample(acc, channel=2)
+
+    f_acc = z_filter.filter(acc)
+    orientation.addSample(f_acc, channel=2)
+    filtered.addSample(f_acc, channel=2)
 
 """ Aurdino Data Aquisition """
 board = Arduino(Arduino.AUTODETECT)
@@ -38,6 +65,8 @@ board.analog[0].register_callback(addY)
 board.analog[0].enable_reporting()
 board.analog[1].register_callback(addX)
 board.analog[1].enable_reporting()
+board.analog[2].register_callback(addZ)
+board.analog[2].enable_reporting()
 
 """ Show Real Time Plots """
 rtp.plt.show()
--- Python Code/realtime_plot.py
+++ Python Code/realtime_plot.py
@@ -1,3 +1,4 @@
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,65 +20,9 @@ class RollingBuffer:
     def add(self, v):
         self.data_buffer.append(v)
 
-""" Real-Time Plotter + FFT"""
-class RealtimePlotFFT:
-    def __init__(self, fs, window_time, title, sample_limits=[-2.5, 2.5], fft_limits=[0,50], fs_calc=False):
-        # Buffer
-        self.buffer_size = fs * window_time
-        self.buffer = RollingBuffer(self.buffer_size)
-
-        # Figure Plot
-        self.fig, self.ax = plt.subplots(nrows=2)
-        
-        # Sample Plot
-        self.ax[0].plot([0, self.buffer_size-1],[0,0],color="r")
-        self.line_r, = self.ax[0].plot(self.buffer.update())
-        self.ax[0].set_ylim(sample_limits[0], sample_limits[1])
-        self.ax[0].set_title(title)
-
-        # FFT Plot
-        fx = np.linspace(0, fs, self.buffer_size)
-        self.line_f, = self.ax[1].plot(fx, self.plot_buffer)
-        self.ax[1].set_ylim(fft_limits[0], fft_limits[1])
-        self.ax[1].set_title(title + " FFT")
-
-        self.anim = animation.FuncAnimation(self.fig, self.update, interval=100)
-        self.update_count = 0
-
-        # Sample Rate
-        self.sample_count = 0
-        self.fs_calc = fs_calc
-        if self.fs_calc:
-            self.label = self.ax[0].text(0, sample_limits[0], "Sample Rate: -", ha="left", va="bottom", fontsize=15)
-            self.last = 0
-
-    def update(self, x):
-        # Buffer
-        self.line_r.set_ydata(self.buffer.update())
-
-        if self.update_count % 5 == 0: # Reduce updates for performance
-            # FFT
-            fft = abs(np.fft.fft(self.plot_buffer))
-            self.line_f.set_ydata(fft)
-
-            # Sample Rate
-            if self.fs_calc:
-                current_time = time.time()
-                sample_rate = self.sample_count / (current_time - self.last)
-                self.last = current_time
-                self.sample_count = 0
-
-                self.label.set_text(f"Fs: {sample_rate:.1f}Hz")
-        self.update_count += 1
-
-    def addSample(self, v):
-        # Add Sample to Buffer
-        self.buffer.add(v)
-        self.sample_count += 1
-
-""" Real-Time Plotter 2 Channel """
+""" Real-Time Plotter Variable Channel """
 class RealtimePlots:
-    def __init__(self, fs, window_time, title, sample_limits=[-2.5, 2.5], channels=1):
+    def __init__(self, fs, window_time, title, sample_limits=[-0.5, 0.5], channels=1):
         # Buffer
         self.buffer_size = fs * window_time
         self.buffers = []
@@ -124,3 +69,25 @@ class RealtimePlots:
         if channel == 0:
             self.sample_count += 1
         self.buffers[channel].add(v)
+
+""" Real-Time Vector Plotter """
+class RealtimeVectorPlot:
+    def __init__(self):
+        self.vec = np.zeros(3)
+
+        self.fig, self.ax = plt.subplots(subplot_kw=dict(projection="3d"))
+        self.ax.set_xlim(-1.5, 1.5)
+        self.ax.set_ylim(-1.5, 1.5)
+        self.ax.set_zlim(-1.5, 1.5)
+
+        self.anim = animation.FuncAnimation(self.fig, self.update, interval=100)
+        self.q = self.ax.quiver(0, 0, 0, 0, 0, 1)
+        
+    def update(self, x):
+        m = np.max(np.abs(self.vec))
+        if m != 0:
+            self.q.remove()
+            self.q = self.ax.quiver(0, 0, 0, self.vec[0] / m, self.vec[1] / m, self.vec[2] / m)
+
+    def addSample(self, v, channel=0):
+        self.vec[channel] = v
